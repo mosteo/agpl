f8c24859dcaa86bfc2df55f3f91a2f67f0c0a316
Alejandro R. Mosteo <amosteo@unizar.es>
2006-09-05 16:23:45 +0000

    A    agpl-generic_indefinite_file_store.adb A    agpl-generic_indefinite_file_store.ads M    agpl-generic_file_store.adb M    agpl-generic_file_store.ads

--- agpl-generic_indefinite_file_store.adb
+++ agpl-generic_indefinite_file_store.adb
@@ -1,6 +1,6 @@
 with Ada.Streams.Stream_Io;
 
-package body Agpl.Generic_File_Store is
+package body Agpl.Generic_Indefinite_File_Store is
 
    -------------
    -- To_File --
@@ -21,26 +21,17 @@ package body Agpl.Generic_File_Store is
    ----------
 
    function Load (File : in String) return Item is
-      Result : Item;
-   begin
-      Load (Result, File);
-
-      return Result;
-   end Load;
-
-   ----------
-   -- Load --
-   ----------
-
-   procedure Load (This : out Item; File : in String) is
       use Ada.Streams.Stream_Io;
       F : File_Type;
    begin
       Open (F, Name => File, Mode => In_File);
 
-      This := Item'Input (Stream (F));
-
-      Close (F);
+      declare
+         Result : constant Item := Item'Input (Stream (F));
+      begin
+         Close (F);
+         return Result;
+      end;
    end Load;
 
-end Agpl.Generic_File_Store;
+end Agpl.Generic_Indefinite_File_Store;
